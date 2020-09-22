0eb0b18048e778574378805053f7a10810359bbe
Víctor Rodríguez Doncel <victorr@ac.upc.edu>
2020-09-22 14:13:18 +0200

    sort of crappy CI

--- src/main/java/es/upm/oeg/protect/rightsearch/RightSearchController.java
+++ src/main/java/es/upm/oeg/protect/rightsearch/RightSearchController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
-        
 
 /**
  * Controller for the REST API service
+ *
  * @author vroddon
  */
 @Controller
@@ -25,11 +25,11 @@ public class RightSearchController {
     ServletContext context;
 
     static Logger logger = Logger.getLogger(Controller.class);
-     
+
     @ApiOperation(value = "Finds references to rights in a collection of ontologies")
     @RequestMapping(
             value = "/rights",
-            produces= "application/json;charset=UTF-8",
+            produces = "application/json;charset=UTF-8",
             method = RequestMethod.GET)
     @ResponseBody
     public String getRights(@RequestParam String text) throws Exception {
@@ -38,19 +38,55 @@ public class RightSearchController {
             String json = RightsFinder.find(text);
             System.out.println("Found: " + json);
             return json;
-                
+
         } catch (Exception e) {
-            logger.error("Error in REST service",e);
+            logger.error("Error in REST service", e);
             logger.error(e.getCause().toString());
         }
         return "{}";
     }
-    
-    @RequestMapping(value ="/status", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/status", method = RequestMethod.GET)
     @ResponseBody
     public String status() {
         System.out.println("I have been asked for the status hahaha");
         return "version 1.0";
     }
-  
+
+    @RequestMapping(value = "/deploy", method = RequestMethod.GET)
+    @ResponseBody
+    public String deploy() {
+        System.out.println("Deploying!");
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("/protect/deploy.sh");
+        try {
+
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Success!");
+                System.out.println(output);
+//                System.exit(0);
+            } else {
+                //abnormal...
+                System.err.println("error " + exitVal);
+            }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        return "Deployed";
+    }
+
 }
