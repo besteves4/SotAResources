b7c232457dd489bfd7db32664f09e6cff9e5b71b
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2020-12-26 01:08:18 +0100

    add controller to deal with /error mapping

---
+++ src/main/java/es/upm/oeg/protect/rightsearch/WebApplicationConfig.java
@@ -0,0 +1,29 @@
+package es.upm.oeg.protect.rightsearch;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+@Override
+public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/notFound").setViewName("forward:/index.html");
+}
+
+
+@Bean
+public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+    return container -> {
+        container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+                "/notFound"));
+    };
+  }
+
+}
