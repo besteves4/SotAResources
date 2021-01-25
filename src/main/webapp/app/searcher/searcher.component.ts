272759645739da3b63de82d9988f43c84df09359
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2021-01-25 02:00:50 +0100

    call rights service in angular

--- src/main/webapp/app/searcher/searcher.component.ts
+++ src/main/webapp/app/searcher/searcher.component.ts
@@ -11,17 +11,31 @@ export class SearcherComponent implements OnInit {
   ngOnInit() { }
 
   title = 'webapp';
-  result = '';
+  status = '';
+  results = {};
 
   constructor(private http: HttpClient){
   }
 
-  public testApp(): void {
-    this.result = 'loading....';
-    this.http.get('/sota/status', { responseType: 'text' }).subscribe((data: string) => {
-      console.log(data);
-      this.result = data;
-    });
+  public getStatus(): void {
+    this.status = 'loading....';
+    this.http.get('/sota/status', { responseType: 'text' })
+      .subscribe((data: string) => {
+        console.log(data);
+        this.status = data;
+      });
+  }
+  
+  public getRights(): void {
+    const params = {
+      text: 'access'
+    }
+
+    this.http.get('/sota/rights', { params: params, responseType: 'json' })
+      .subscribe((data: any) => {
+        console.log('test ', data);
+        this.results = data;
+      });
   }
 
 }
